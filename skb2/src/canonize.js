4470da14416817c716cbb9b0cc913e35ba329f62
Oleksandr Piro <stayer8@i.ua>
2016-11-19 22:57:02 +0200

    '3rd'

--- skb2/src/canonize.js
+++ skb2/src/canonize.js
@@ -1,7 +1,8 @@
 export default function canonize(url) {
-  const re = new RegExp("@?(https?:)?(//)?((www\.)?(vk||twitter)[^/]*/)?([a-zA-Z0-9_-]*)(\.?([a-zA-Z0-9_-]*)?)?(/*)?");
+  const re = new RegExp("@?(https?:)?(//)?((www\.)?(vk||twitter)[^/]*/)?([a-zA-Z0-9_-]*)|(\.?([a-zA-Z0-9_-]*)?)?(/*)?");
   const username = url.match(re)[6];
   return '@' + username;
 }
 
-//Застрял между 9 и 10 тестами. Проходит либо https://github.com/kriasoft/react-starter-kit (данный вариант), либо username=vk.com/pavel.durov
+//Застрял между 9 и 10 тестами. Проходит либо ?username=https://github.com/kriasoft/react-starter-kit (данный вариант), либо ?username=vk.com/pavel.durov
+//("@?(https?:)?(//)?((www\.)?(vk||twitter)[^/]*/)?([a-zA-Z0-9_-]*)(\.?([a-zA-Z0-9_-]*)?)?(/*)?")
